51d34de5b1cadef5bf6e1c8735c4b8850ea10ac2
Vincent C <vincent.chung@kellogg.northwestern.edu>
2022-05-18 19:10:30 +0000

    Add City function

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,16 @@
+class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+      end
+    def show
+        @place = Place.find_by({ "id" => params["id"] })
+    end
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+      end
+    def new
+        @place = Place.new
+    end
+end
