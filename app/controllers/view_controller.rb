b8c5a43fa8efa76cea498a1b3e78e8bf3959f704
Vincent C <vincent.chung@kellogg.northwestern.edu>
2022-05-18 19:36:35 +0000

    update

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -2,5 +2,11 @@ class ViewController < ApplicationController
     def new
         @post = Post.new
         @post.place_id = params["place_id"]
+        @place = Place.find_by({ "id" => params["place_id"] })
+    end
+    def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places/#{@post.place_id}"
       end
 end
